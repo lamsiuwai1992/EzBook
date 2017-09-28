d557698e2ed58d3bf878031c14b17cfc7497caa7
hohilary <hohilary@yahoo.com.hk>
2017-09-28 14:37:14 +0800

    Add button to do logout function

--- app/src/main/java/com/ezbook/lamsiuwai/ezbook/ProfileFragment.java
+++ app/src/main/java/com/ezbook/lamsiuwai/ezbook/ProfileFragment.java
@@ -2,15 +2,25 @@ package com.ezbook.lamsiuwai.ezbook;
 
 /**
  * Created by lamsiuwai on 18/9/2017.
+ * Edited by hilary on 25/09/2017
  */
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ProfileFragment extends Fragment {
+
+    private Button btnLogout;
+
     public static ProfileFragment newInstance() {
         ProfileFragment fragment = new ProfileFragment();
         return fragment;
@@ -22,8 +32,19 @@ public class ProfileFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+    public View onCreateView(LayoutInflater inflater, final ViewGroup container, Bundle savedInstanceState) {
+        final View view =inflater.inflate(R.layout.fragment_profile, container, false);
+
+        btnLogout = view.findViewById(R.id.btn_logout);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 }
