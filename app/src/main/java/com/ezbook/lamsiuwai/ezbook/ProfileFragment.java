86bed94a8290f00053847bb616afedc1da0d007a
lamsiuwai1992 <lamsiuwai1992@gmail.com>
2017-11-26 17:57:07 +0800

    fix bugs when pressing back

--- app/src/main/java/com/ezbook/lamsiuwai/ezbook/ProfileFragment.java
+++ app/src/main/java/com/ezbook/lamsiuwai/ezbook/ProfileFragment.java
@@ -330,6 +330,7 @@ public class ProfileFragment extends Fragment {
             public void onClick(View v) {
                 FirebaseAuth.getInstance().signOut();
                 Intent intent = new Intent(getActivity(), LoginActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(intent);
             }
         });
